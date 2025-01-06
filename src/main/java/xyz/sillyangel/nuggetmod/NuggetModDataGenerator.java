06b1affe7fc0c692ccd55ba26c80a340caf2b026
sillyangel <sillyangel3@gmail.com>
2025-01-06 13:47:10 -0600

    after datagen

--- src/main/java/xyz/sillyangel/nuggetmod/NuggetModDataGenerator.java
+++ src/main/java/xyz/sillyangel/nuggetmod/NuggetModDataGenerator.java
@@ -2,10 +2,17 @@ package xyz.sillyangel.nuggetmod;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import xyz.sillyangel.nuggetmod.datagen.*;
 
 public class NuggetModDataGenerator implements DataGeneratorEntrypoint {
-	@Override
-	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
-	}
+        pack.addProvider(ModBlockTagProvider::new);
+        pack.addProvider(ModItemTagProvider::new);
+        pack.addProvider(ModLootTableProvider::new);
+        pack.addProvider(ModModelProvider::new);
+        pack.addProvider(ModRecipeProvider::new);
+    }
 }
---
+++ src/main/java/xyz/sillyangel/nuggetmod/datagen/ModBlockTagProvider.java
@@ -0,0 +1,30 @@
+package xyz.sillyangel.nuggetmod.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import xyz.sillyangel.nuggetmod.block.ModBlocks;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.BlockTags;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
+    public ModBlockTagProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
+        getOrCreateTagBuilder(BlockTags.PICKAXE_MINEABLE)
+                .add(ModBlocks.NUGGET_BLOCK)
+                .add(ModBlocks.RAW_NUGGET_BLOCK)
+                .add(ModBlocks.NUGGET_ORE)
+                .add(ModBlocks.NUGGET_DEEPSLATE_ORE);
+
+        getOrCreateTagBuilder(BlockTags.NEEDS_STONE_TOOL)
+                .add(ModBlocks.NUGGET_DEEPSLATE_ORE)
+                .add(ModBlocks.NUGGET_BLOCK)
+                .add(ModBlocks.RAW_NUGGET_BLOCK)
+                .add(ModBlocks.NUGGET_ORE);
+    }
+}
