bacde6545f348c8b50c0f2e00e36c8d6ea209c7a
sillyangel <sillyangel3@gmail.com>
2025-01-05 23:23:43 -0600

    First Commit

---
+++ src/main/java/xyz/sillyangel/nuggetmod/NuggetMod.java
@@ -0,0 +1,16 @@
+package xyz.sillyangel.nuggetmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NuggetMod implements ModInitializer {
+	public static final String MOD_ID = "nuggetmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/xyz/sillyangel/nuggetmod/NuggetModDataGenerator.java
@@ -0,0 +1,11 @@
+package xyz.sillyangel.nuggetmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class NuggetModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
